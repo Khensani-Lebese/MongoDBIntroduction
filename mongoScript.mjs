41eeaee7ef72f95ec0d0c282d52afcbe0edb1290
Khensani-Lebese <Khensanilebese0@gmail.com>
2025-02-01 02:57:33 +0200

    fixed the 'cddb is not defined' error

--- mongoScript.mjs
+++ mongoScript.mjs
@@ -1,37 +1,34 @@
 //create database
-use ("Codetribe");
+use("Codetribe");
 
 //create documents
- db.createCollection("Facilitators");
+db.createCollection("Facilitators");
 
- db.createCollection("Trainees");
+db.createCollection("Trainees");
 
 db.createCollection("Projects");
 
-
 //create documents
 db.Facilitators.insertOne({
- Name: "John Doe",
- Location: "Johannesburg",
-  Course: "Full Stack Development"
- })
+  Name: "John Doe",
+  Location: "Johannesburg",
+  Course: "Full Stack Development",
+});
 
+db.Facilitators.insertOne({
+  Name: "Vukona Mnisi",
+  Location: "Pretoria",
+  Course: "React Native",
+});
 
-
- db.Facilitators.insertOne({ 
-    Name: "Vukona Mnisi", 
-    Location: "Pretoria", 
-    Course: "React Native" }
- )
-
- db.Trainees.insertOne({ 
-    Name: "Khensani Lebese",
-     Location: "Pretoria", 
-     Facilitator: "Vukona mnisi" } )
-
+db.Trainees.insertOne({
+  Name: "Khensani Lebese",
+  Location: "Pretoria",
+  Facilitator: "Vukona mnisi",
+});
 
 //read the database
- db.Facilitators.find().pretty();
+db.Facilitators.find().pretty();
 
- cddb.Trainees.find().pretty();
- db.Projects.find().pretty();
+db.Trainees.find().pretty();
+db.Projects.find().pretty();
